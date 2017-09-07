12d9867b3b32215f0480c10ce56fef115dd8aa24
Bentivimon <88nazar88@gmail.com>
2017-09-07 19:56:25 +0300

    Create project for home game

---
+++ PrismLab_1/PrismLab_1/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using PrismLab_1.Views;
+using System.Windows;
+
+namespace PrismLab_1
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ PrismLab_1/PrismLab_1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+
+namespace PrismLab_1.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        public string Question
+        {
+            get { return _question; }
+            set { SetProperty(ref _question, value); }
+        }
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private string _title = "Prism Unity Application";
+        private string _question = "Test Text";
+
+    }
+}
